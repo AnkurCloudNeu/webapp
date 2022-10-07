3fce717865de78e9c3d5d8e47e61b002509f471b
Ankur Thakur <99705200+Ankur-Thakur-NEU@users.noreply.github.com>
2022-10-06 23:48:04 -0400

    Guid a2 01 (#6)
    * Moved readme outside of cloud api
    
    * Added Guid in response

--- WebApp.CloudApi/ResponseModel/AccountResponse.cs
+++ WebApp.CloudApi/ResponseModel/AccountResponse.cs
@@ -1,6 +1,7 @@
 namespace WebApp.CloudApi.ResponseModel;
 public class AccountResponse
 { 
+    public Guid AccountId { get; set; }
     public string FirstName { get; set; } = string.Empty;
     public string LastName { get; set; } = string.Empty;
     public string Email { get; set; } = string.Empty;
