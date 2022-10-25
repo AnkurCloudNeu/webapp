f965292e16fc427fa9222cbd8a2a8061aca5ca22
Ankur Thakur <99705200+Ankur-Thakur-NEU@users.noreply.github.com>
2022-10-25 15:46:00 -0400

    A5 01 (#19)
    * added hcl file
    
    * added packer into repo
    
    * added packer.yml into github actions
    
    * fixed packer
    
    * fixed file name
    
    * fixed packer hcl
    
    * Added port number
    
    * Changed url
    
    * Added production url
    
    * undo changes
    
    * fixed file directory issue
    
    * Fixed error
    
    * added looging to application

--- WebApp.CloudApi/Program.cs
+++ WebApp.CloudApi/Program.cs
@@ -5,9 +5,16 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.OpenApi.Models;
 using Microsoft.EntityFrameworkCore;
 using WebApp.CloudApi.Helper;
+using Serilog;
+using Serilog.Events;
 
 var builder = WebApplication.CreateBuilder(args);
-
+Log.Logger = new LoggerConfiguration()
+           .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+           .Enrich.FromLogContext()
+           .WriteTo.Console()
+           .CreateLogger();
+Log.Information("Starting web host");
 // Add services to the container.
 // Add services to the container.
 builder.Services.AddDbContext<EF_DataContext>(
@@ -50,7 +57,10 @@ builder.Services.AddSwaggerGen(options =>
     // var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
     // options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
 });
-
+var logger = new LoggerConfiguration().ReadFrom.
+Configuration(builder.Configuration).Enrich.FromLogContext().CreateLogger();
+builder.Logging.ClearProviders();
+builder.Logging.AddSerilog(logger);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -67,9 +77,12 @@ app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
+app.UseMiddleware(typeof(ExceptionHandlingMiddleware));
 if (app.Environment.IsDevelopment())
 {
     app.Run();
-} else {
+}
+else
+{
     app.Run("http://0.0.0.0:8080");
 }
