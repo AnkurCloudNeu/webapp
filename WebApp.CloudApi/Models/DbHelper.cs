8258886f3cecef2a918eac5d8154c39b6f2bae59
Ankur Thakur <99705200+Ankur-Thakur-NEU@users.noreply.github.com>
2022-10-26 21:57:14 -0400

    A5 12 (#32)
    * Fixed connection string
    
    * Connection string moved
    
    * Undo logging
    
    * Added logs again
    
    * changed connection string
    
    * Fixed Serilog
    
    * Undo log changes
    
    * Undo launch changes
    
    * Fixed logs
    
    * fixed
    
    * test
    
    * FIxed errors
    
    * Fixed Encryption
    
    * removed errors
    
    * fixed  boundary condition
    
    * FIxed config value

--- WebApp.CloudApi/Models/DbHelper.cs
+++ WebApp.CloudApi/Models/DbHelper.cs
@@ -5,23 +5,28 @@ using WebApp.CloudApi.ResponseModel;
 
 namespace WebApp.CloudApi.Model;
 
-public class DbHelper {
+public class DbHelper
+{
     private EF_DataContext _context;
-     private readonly ApplicationInstance _application;
-    
+    string key = "E546C8DF278CD5931069B522E695D4F2";
+    private readonly ApplicationInstance _application;
+
     private readonly IConfiguration _config;
-    
-    public DbHelper(EF_DataContext context, IConfiguration config, 
-        ApplicationInstance application) {
+
+    public DbHelper(EF_DataContext context, IConfiguration config,
+        ApplicationInstance application)
+    {
         _context = context;
         _config = config;
         _application = application;
     }
 
-    public AccountResponse GetAccount(Guid id) {
-        var account = _context.Accounts.Where(m => m.AccountID.Equals(id) 
+    public AccountResponse GetAccount(Guid id)
+    {
+        var account = _context.Accounts.Where(m => m.AccountID.Equals(id)
         && m.AccountID.Equals(_application.Application)).Single();
-        return new AccountResponse {
+        return new AccountResponse
+        {
             AccountId = account.AccountID,
             Email = account.Email,
             FirstName = account.FirstName,
@@ -31,25 +36,30 @@ public class DbHelper {
         };
     }
 
-    public bool GetAccount(string email, string password) {
+    public bool GetAccount(string email, string password)
+    {
         var account = _context.Accounts.Where(m => m.Email.Equals(email)).Single();
         _application.Application = account.AccountID;
-        string decryptedPassword = EncryptDecrypt.DecryptString(account.Password, _config.GetValue<string>("Salt"));
+        string decryptedPassword = EncryptDecrypt.DecryptString(account.Password, key);
         return password == decryptedPassword;
     }
 
-    public Account GetAccount(string email) {
-        if (_context.Accounts.ToList().Count == 0) {
+    public Account GetAccount(string email)
+    {
+        if (_context.Accounts.ToList().Count == 0)
+        {
             return new Account();
         }
         var account = _context.Accounts.Where(m => m.Email.Equals(email)).First();
-        account.Password = EncryptDecrypt.DecryptString(account.Password, _config.GetValue<string>("Salt"));
+        account.Password = EncryptDecrypt.DecryptString(account.Password, key);
         return account;
     }
 
-    public async Task<AccountResponse> SaveAccount(AccountRequest request) {
-        request.Password = EncryptDecrypt.EncryptString(request.Password, _config.GetValue<string>("Salt"));
-        Account account = new Account {
+    public async Task<AccountResponse> SaveAccount(AccountRequest request)
+    {
+        request.Password = EncryptDecrypt.EncryptString(request.Password, key);
+        Account account = new Account
+        {
             FirstName = request.FirstName,
             LastName = request.LastName,
             Email = request.Email,
@@ -60,7 +70,8 @@ public class DbHelper {
 
         await _context.Accounts.AddAsync(account);
         await _context.SaveChangesAsync();
-        return new AccountResponse {
+        return new AccountResponse
+        {
             AccountId = account.AccountID,
             Email = account.Email,
             FirstName = account.FirstName,
@@ -70,18 +81,23 @@ public class DbHelper {
         };
     }
 
-     public async Task<AccountResponse> UpdateAccount(Guid id, AccountRequest request) {
+    public async Task<AccountResponse> UpdateAccount(Guid id, AccountRequest request)
+    {
         Account account = _context.Accounts.Where(m => m.AccountID.Equals(id)).First();
-        if(account.AccountID == new Guid()) {
+        if (account.AccountID == new Guid())
+        {
             return new AccountResponse();
-        } else {
-            account.Password = EncryptDecrypt.EncryptString(request.Password, _config.GetValue<string>("Salt"));
+        }
+        else
+        {
+            account.Password = EncryptDecrypt.EncryptString(request.Password, key);
             account.FirstName = request.FirstName;
             account.LastName = request.LastName;
             account.AccountUpdated = DateTime.UtcNow;
         }
         await _context.SaveChangesAsync();
-        return new AccountResponse {
+        return new AccountResponse
+        {
             AccountId = account.AccountID,
             Email = account.Email,
             FirstName = account.FirstName,
